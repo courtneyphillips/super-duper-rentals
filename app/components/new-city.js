82ac9e565e103ab8e798bcc605aed2fb05419dbf
Courtney Mae Phillips <courtney.mae.phillips@gmail.com>
2015-09-27 18:51:05 -0700

    Generate new-city component, reference in index template.

--- app/components/new-city.js
+++ app/components/new-city.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewCity: false,
+  actions: {
+    cityFormShow() {
+      this.set('addNewCity', true);
+    },
+    cityFormHide() {
+      this.set('addNewCity', false);
+    },
+    saveCity() {
+      var params = {
+        name: this.get('name'),
+        attractions: this.get('attractions'),
+        country: this.get('country')
+      };
+      this.sendAction('saveCity', params);
+      this.set('adNewCity', false);
+    }
+  }
 });
