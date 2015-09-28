21b9dedd84f0197051854fff192a7e17262f0a3e
Courtney Mae Phillips <courtney.mae.phillips@gmail.com>
2015-09-27 18:37:08 -0700

    Fix bug preventing application.hbs from rendering, generate city model.

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('rental', params.rental_id);
+  },
+  delete(rental) {
+    rental.destroyRecord();
+    this.transitionTo('index');
   }
 });
